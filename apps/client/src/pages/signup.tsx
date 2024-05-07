693a777adf7bff8f20a3addecaf6f47b8d5162da
Mohit Namdev <namdevmohit0@gmail.com>
2024-05-07 12:01:35 +0530

    installed MUI

--- apps/client/src/pages/signup.tsx
+++ apps/client/src/pages/signup.tsx
@@ -1,7 +1,57 @@
+import { Button, Card, TextField, Typography } from "@mui/material";
 import {Signup} from "../../../../packages/ui/components"
 
 export default function SigninPage(){
     return<div>
-        <Signup/>
+            <div style={{
+                paddingTop: 150,
+                marginBottom: 10,
+                display: "flex",
+                justifyContent: "center"
+            }}>
+                <Typography variant={"h6"}>
+                Welcome to Coursera. Sign up below
+                </Typography>
+            </div>
+        <div style={{display: "flex", justifyContent: "center"}}>
+            <Card varint={"outlined"} style={{width: 400, padding: 20}}>
+                <TextField
+                    onChange={(event) => {
+                        setEmail(event.target.value);
+                    }}
+                    fullWidth={true}
+                    label="Email"
+                    variant="outlined"
+                />
+                <br/><br/>
+                <TextField
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                    fullWidth={true}
+                    label="Password"
+                    variant="outlined"
+                    type={"password"}
+                />
+                <br/><br/>
+
+                <Button
+                    size={"large"}
+                    variant="contained"
+                    onClick={async() => {
+                        const response = await axios.post(`${BASE_URL}/admin/signup`, {
+                            username: email,
+                            password: password
+                        })
+                        let data = response.data;
+                        localStorage.setItem("token", data.token);
+                        // window.location = "/"
+                        setUser({userEmail: email, isLoading: false})
+                        navigate("/courses")
+                    }}
+
+                > Signup</Button>
+            </Card>
+        </div>
     </div>
 }
